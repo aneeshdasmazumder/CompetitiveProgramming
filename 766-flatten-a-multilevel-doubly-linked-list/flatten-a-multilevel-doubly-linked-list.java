5105d6e88675283d67a26d57db1edf462cdb2354
Aneesh Das Mazumder <47806819+aneeshdasmazumder@users.noreply.github.com>
2023-09-14 13:41:30 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (75.04%) - LeetSync

--- 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.java
+++ 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.java
@@ -9,31 +9,54 @@ class Node {
 */
 
 class Solution {
+    public Node reachTail(Node tail) {
+        while(tail.next != null) {
+            tail = tail.next;
+        }
+
+        return tail;
+    }
     public Node flatten(Node head) {
 
         Node curr;
-        Stack<Node> stk = new Stack<>();
+        // Stack<Node> stk = new Stack<>();
 
         curr = head;
 
-        while(curr != null) {
+        while(curr!= null) {
             if(curr.child != null) {
-                if(curr.next != null)
-                    stk.push(curr.next);
-                Node temp = curr;
+                Node tail = reachTail(curr.child);
+                tail.next = curr.next;
+                if(tail.next != null)
+                    tail.next.prev = tail;
+                tail = tail.next;
                 curr.next = curr.child;
+                curr.next.prev = curr;
                 curr.child = null;
-                curr = curr.next;
-                curr.prev = temp;
-            } else if(curr.next == null && !stk.isEmpty()) {
-                
-                Node lastNode = curr;
-                curr.next = stk.pop();
-                curr = curr.next;
-                curr.prev = lastNode;
-                
-            } else curr = curr.next;
+            } 
+            curr = curr.next;
         }
+
+        
+
+        // while(curr != null) {
+        //     if(curr.child != null) {
+        //         if(curr.next != null)
+        //             stk.push(curr.next);
+        //         Node temp = curr;
+        //         curr.next = curr.child;
+        //         curr.child = null;
+        //         curr = curr.next;
+        //         curr.prev = temp;
+        //     } else if(curr.next == null && !stk.isEmpty()) {
+                
+        //         Node lastNode = curr;
+        //         curr.next = stk.pop();
+        //         curr = curr.next;
+        //         curr.prev = lastNode;
+                
+        //     } else curr = curr.next;
+        // }
 
         return head;
         
