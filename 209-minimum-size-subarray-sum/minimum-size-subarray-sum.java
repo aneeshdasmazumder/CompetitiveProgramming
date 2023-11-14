028ac2e8698a82a2f89be66289b054318f929123
Aneesh Das Mazumder <47806819+aneeshdasmazumder@users.noreply.github.com>
2023-11-14 15:32:18 +0530

    Time: 1 ms (99.94%) | Memory: 54.8 MB (60.44%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -4,9 +4,10 @@ class Solution {
         int right = 0;
         int minLen = Integer.MAX_VALUE;
         int currSum = 0;
-
+        // loop over the array with sliding window
         for(; right<nums.length; right++) {
             currSum = currSum + nums[right];
+            // take the length and shorten the windown from left
             while (currSum >= target) {
                 int len = (right - left) + 1;
 
@@ -14,7 +15,6 @@ class Solution {
                 currSum -= nums[left];
                 left++;
             }
-            
         }
 
         if(minLen == Integer.MAX_VALUE)
