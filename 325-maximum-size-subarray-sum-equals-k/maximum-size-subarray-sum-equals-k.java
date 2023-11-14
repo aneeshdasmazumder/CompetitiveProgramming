088f218b9084880b0ba2c7d5ce516fad1d1e14df
Aneesh Das Mazumder <47806819+aneeshdasmazumder@users.noreply.github.com>
2023-11-14 14:37:57 +0530

    Time: 54 ms (82.66%) | Memory: 70.6 MB (34.69%) - LeetSync

--- 325-maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.java
+++ 325-maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.java
@@ -3,15 +3,18 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         int maxLen = 0;
         int currSum = 0;
-
+        // loop over the array
         for(int i=0; i<nums.length; i++) {
+            // calculate the curr sum one after another
             currSum += nums[i];
 
+            // 1. If currSum matches the k, then calculate the maxLen
             if(currSum == k) maxLen = i + 1;
+            // 2. If totalSumof the array - given value present in the map then get maxLen
             else if(map.containsKey(currSum - k)) {
                 maxLen = Math.max(maxLen, i - map.get(currSum - k));
             }
-
+            // If the currSum not present store it with index
             if(!map.containsKey(currSum))
                 map.put(currSum, i);
         }
