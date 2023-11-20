0448de4d37e24acc49352f37e6adafb015536e76
Aneesh Das Mazumder <47806819+aneeshdasmazumder@users.noreply.github.com>
2023-11-20 23:53:03 +0530

    Time: 1 ms (98.95%) | Memory: 59.9 MB (14.78%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,13 +1,18 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
         for(int i=0; i<nums.length; i++) {
-
+            // Cases to check
+            // 1. Arrange the values in the loop according to given value array based index
+            // 2. Check only if...
+            //  2.1> ith value - 1 is not equal to i
+            //  2.2> ith value is withing the range of 0 to n
+            //  2.3> current index value is not equal to the value in the already present index
             while(nums[i]-1 != i && nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[nums[i]-1]) {
                 int temp = nums[nums[i] - 1];
                 nums[nums[i] - 1] = nums[i];
                 nums[i] = temp;
             }
-            System.out.println(nums[i]);
+            // System.out.println(nums[i]);
         }
 
         int val = Integer.MAX_VALUE;
