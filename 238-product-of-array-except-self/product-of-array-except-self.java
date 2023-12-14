17a5ace4e5be095ae1ec8c550aa08554667abe9b
Aneesh Das Mazumder <47806819+aneeshdasmazumder@users.noreply.github.com>
2023-12-15 01:06:13 +0530

    Time: 2 ms (64.37%) | Memory: 51.7 MB (77.63%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,44 +1,44 @@
 class Solution {
     
     public int[] productExceptSelf(int[] nums) {
-        // int[] left = new int[nums.length];
-        // int[] ans = new int[nums.length];
+        int[] left = new int[nums.length];
+        int[] ans = new int[nums.length];
 
-        // left[0] = 1;
-        // for(int i=1; i<nums.length; i++) {
-        //     left[i] = left[i-1] * nums[i-1];
-        // }
-
-        // ans[nums.length-1] = left[left.length-1];
-        // int temp = 1;
-        // for(int i=ans.length-2; i>=0; i--) {
-        //     temp = temp * nums[i+1];
-        //     ans[i] = temp * left[i];
-        // }
-        // return ans;
-        int n = nums.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
-        int[] ans = new int[n];
-
-        left[0] = nums[0];
-        for(int i=1; i<n; i++) {
-            left[i] = left[i-1] * nums[i];
+        left[0] = 1;
+        for(int i=1; i<nums.length; i++) {
+            left[i] = left[i-1] * nums[i-1];
         }
 
-        right[n-1] = nums[n-1];
-
-        for(int i=n-2; i>=0; i--) {
-            right[i] = right[i+1] * nums[i];
+        ans[nums.length-1] = left[left.length-1];
+        int temp = 1;
+        for(int i=ans.length-2; i>=0; i--) {
+            temp = temp * nums[i+1];
+            ans[i] = temp * left[i];
         }
-
-        ans[0] = right[1];
-        ans[n-1] = left[n-2];
-
-        for(int i=n-2; i>0; i--) {
-            ans[i] = left[i-1] * right[i+1];
-        }
-
         return ans;
+        // int n = nums.length;
+        // int[] left = new int[n];
+        // int[] right = new int[n];
+        // int[] ans = new int[n];
+
+        // left[0] = nums[0];
+        // for(int i=1; i<n; i++) {
+        //     left[i] = left[i-1] * nums[i];
+        // }
+
+        // right[n-1] = nums[n-1];
+
+        // for(int i=n-2; i>=0; i--) {
+        //     right[i] = right[i+1] * nums[i];
+        // }
+
+        // ans[0] = right[1];
+        // ans[n-1] = left[n-2];
+
+        // for(int i=n-2; i>0; i--) {
+        //     ans[i] = left[i-1] * right[i+1];
+        // }
+
+        // return ans;
     }
 }
