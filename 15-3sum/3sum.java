42d725070895b64552cd968be93b8730afc397cb
Aneesh Das Mazumder <47806819+aneeshdasmazumder@users.noreply.github.com>
2023-12-20 22:52:42 +0530

    Time: 28 ms (84.19%) | Memory: 51 MB (39.54%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,74 +1,38 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        Map<Integer, Integer> map = new HashMap<>();
-        // for(int i=0; i<nums.length; i++) {
-        //     for(int j=i+1; j<nums.length; j++) {
-        //         for(int k=j+1; k<nums.length; k++) {
-        //             if(nums[i] + nums[j] + nums[k] == 0) {
-        //                 temp.add(nums[i]);
-        //                 temp.add(nums[j]);
-        //                 temp.add(nums[k]);
-        //                 result.add(temp);
-        //                 temp = new ArrayList<>();
-        //             }
-        //         }
-        //     }
-        // }
-
-        for(int i=0; i<nums.length; i++) {
-            if(!map.containsKey(nums[i])) {
-                map.put(nums[i], 1);
-            } else {
-                map.put(nums[i], map.get(nums[i])+1);
-            }
-        }
-
-        // int left = 0;
-        // int right = nums.length - 1;
         Arrays.sort(nums);
-        // while(left < right) {
-        //     int value = nums[left] + nums[right];
-        //     if(map.containsKey((0-value)) && map.get(0-value) != 1) {
-        //         System.out.println((0-value) + " " + nums[left] + " " + nums[right]);
-        //         temp.add(0-value);
-        //         temp.add(nums[left]);
-        //         temp.add(nums[right]);
-        //         result.add(temp);
-        //         temp = new ArrayList<>();
-        //         if(map.get(0-value) == 1) map.remove(0-value);
-        //         else map.put(0-value, map.get(0-value) - 1);
-        //     }
-
-        //     if(value <= 0) left++;
-        //     else right--;
-        // }
-        Set<List<Integer>> set = new HashSet<>();
-        for(int i=0; i<nums.length; i++) {
-            int left = i+1;
-            int right = nums.length -1;
-
-            while (left < right) {
-                if(nums[i] + nums[left] + nums[right] == 0) {
-                    temp.add(nums[i]);
-                    temp.add(nums[left]);
-                    temp.add(nums[right]);
-                    set.add(temp);
-                    temp = new ArrayList<>();
-                }
-
-                if(nums[left] + nums[right] + nums[i] <= 0) left++;
-                else right--;
+        List<List<Integer>> answer = new ArrayList<>();
+        
+     
+        
+        for (int i = 0; i < nums.length; ++i) {
+          
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
             }
-
             
+            int low = i + 1, high = nums.length - 1;
+            while (low < high) {
+                int sum = nums[i] + nums[low] + nums[high];
+                if (sum > 0) {
+                    high--;
+                } else if (sum < 0) {
+                    low++;
+                } else {
+                    answer.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                    int lastLowOccurrence = nums[low];
+                    int lastHighOccurrence = nums[high];
+                    
+                    while (low < high && nums[low] == lastLowOccurrence) {
+                        low++;
+                    }
+                    
+                    while (low < high && nums[high] == lastHighOccurrence) {
+                        high--;
+                    }
+                }
+            }
         }
-
-        for(List l : set) {
-            result.add(l);
-        }
-
-        return result;
+        return answer;
     }
 }
