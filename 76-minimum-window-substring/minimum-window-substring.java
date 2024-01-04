2e131ccdc2b45414c0ab6c98784efe5c9321d1a2
Aneesh Das Mazumder <47806819+aneeshdasmazumder@users.noreply.github.com>
2024-01-05 00:19:30 +0530

    Time: 14 ms (56.97%) | Memory: 45.2 MB (14.49%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,46 +1,53 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(t.length() > s.length()) return "";
+        if (t.length() > s.length())
+            return "";
         // Define a HashMap to store the unique chars and count
         Map<Character, Integer> uniqueChar = new HashMap<>();
 
         int count = t.length();
 
-        for(int i=0; i<count; i++) {
-            if(!uniqueChar.containsKey(t.charAt(i))) {
+        for (int i = 0; i < count; i++) {
+            if (!uniqueChar.containsKey(t.charAt(i))) {
                 uniqueChar.put(t.charAt(i), 1);
             } else {
                 uniqueChar.put(t.charAt(i), uniqueChar.get(t.charAt(i)) + 1);
             }
         }
 
-        int left = 0; int right = 0; int l = -1; int r = -1;
+        int left = 0;
+        int right = 0;
+        int l = -1;
+        int r = -1;
         int minLength = s.length();
         int len = 0;
         String resultStr = "";
-        while(left <= right && right < s.length()) {
+        while (left <= right && right < s.length()) {
             // System.out.println(count + " " + s.charAt(right));
 
-            if(uniqueChar.containsKey(s.charAt(right))) {
+                // Check if right char is present in the map and decrease.
+            if (uniqueChar.containsKey(s.charAt(right))) {
                 uniqueChar.put(s.charAt(right), uniqueChar.get(s.charAt(right)) - 1);
-                if(uniqueChar.get(s.charAt(right)) >= 0)
+
+                // Decrease count if value is >=0
+                if (uniqueChar.get(s.charAt(right)) >= 0)
                     count--;
             }
 
-            
-            while(count == 0) {
-                
-                if(uniqueChar.containsKey(s.charAt(left))) {
+            // Handling the left pointer and getting the minimum length
+            while (count == 0) {
+
+                if (uniqueChar.containsKey(s.charAt(left))) {
                     uniqueChar.put(s.charAt(left), uniqueChar.get(s.charAt(left)) + 1);
-                    if(uniqueChar.get(s.charAt(left)) > 0)
+                    if (uniqueChar.get(s.charAt(left)) > 0)
                         count++;
                 }
 
-                if(count > 0) {
+                if (count > 0) {
                     int diff = right - left;
-                    if(diff < minLength) {
+                    if (diff < minLength) {
                         l = left;
-                        r = right+1;
+                        r = right + 1;
                         minLength = diff;
                     }
                 }
@@ -49,12 +56,12 @@ class Solution {
             }
 
             right++;
-            
+
         }
         // System.out.println(l + " " + r);
-        if(l >=0 && r >= 0)
+        if (l >= 0 && r >= 0)
             resultStr = s.substring(l, r);
-        
+
         return resultStr;
     }
 }
