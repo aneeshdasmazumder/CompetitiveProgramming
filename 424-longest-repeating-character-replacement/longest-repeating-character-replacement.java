6e5304dd804a1b871a9501bd4017da56a74bcb21
Aneesh Das Mazumder <47806819+aneeshdasmazumder@users.noreply.github.com>
2024-01-07 00:04:55 +0530

    Time: 31 ms (21.74%) | Memory: 44.4 MB (10.79%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -5,17 +5,23 @@ class Solution {
         int result = 1;
         int left = 0, right = 0;
 
+        // Loop from left -> right
         while(right < s.length()) {
+
+            // Store the frequency
             if(!map.containsKey(s.charAt(right))) {
                 map.put(s.charAt(right), 1);
             } else {
                 map.put(s.charAt(right), map.get(s.charAt(right)) + 1);
             }
 
+            // Store the most frequent char
             maxFreq = Math.max(maxFreq, map.get(s.charAt(right)));
 
+            // Check is number of exchange can be done
             if(((right - left) + 1) - maxFreq <= k) result = Math.max(result, (right - left) + 1);
 
+            
             while(((right - left) + 1) - maxFreq > k) {
                 map.put(s.charAt(left), map.get(s.charAt(left)) - 1);
                 maxFreq = Math.max(maxFreq, map.get(s.charAt(left)));
