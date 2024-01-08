6b8e826d5f7957bc545c139c76ade8e2c6655063
Aneesh Das Mazumder <47806819+aneeshdasmazumder@users.noreply.github.com>
2024-01-09 00:00:35 +0530

    Time: 7 ms (47.78%) | Memory: 47.8 MB (19.84%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,19 +1,47 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        // Map<String, List<String>> map = new HashMap<>();
+        // List<List<String>> result = new ArrayList<>();
+        // for(int i=0; i<strs.length; i++) {
+        //     char[] str = strs[i].toCharArray();
+        //     Arrays.sort(str);
+        //     String newStr = new String(str);
+        //     if(!map.containsKey(newStr)) {
+        //         List<String> temp = new ArrayList<>();
+        //         temp.add(strs[i]);
+        //         map.put(newStr, temp);
+        //     } else {
+        //         List<String> temp = map.get(newStr);
+        //         temp.add(strs[i]);
+        //         map.put(newStr, temp);
+        //     }
+        // }
+
+        // Set<String> keys = map.keySet();
+
+        // for(String key : keys) {
+        //     result.add(map.get(key));
+        // }
+
+        // return result;
+
         Map<String, List<String>> map = new HashMap<>();
         List<List<String>> result = new ArrayList<>();
+
         for(int i=0; i<strs.length; i++) {
-            char[] str = strs[i].toCharArray();
-            Arrays.sort(str);
-            String newStr = new String(str);
-            if(!map.containsKey(newStr)) {
-                List<String> temp = new ArrayList<>();
-                temp.add(strs[i]);
-                map.put(newStr, temp);
+            char[] ch = strs[i].toCharArray();
+            Arrays.sort(ch);
+
+            String s = new String(ch);
+
+            if(!map.containsKey(s)) {
+                List<String> tempList = new ArrayList<>();
+                tempList.add(strs[i]);
+                map.put(s, tempList);
             } else {
-                List<String> temp = map.get(newStr);
-                temp.add(strs[i]);
-                map.put(newStr, temp);
+                List<String> tempList = map.get(s);
+                tempList.add(strs[i]);
+                map.put(s, tempList);
             }
         }
 
