2b549574169604bd4b1ed643f3e595550debe950
Aneesh Das Mazumder <47806819+aneeshdasmazumder@users.noreply.github.com>
2024-02-12 01:13:46 +0530

    Time: 12 ms (67.11%) | Memory: 48.4 MB (52.67%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
@@ -3,23 +3,63 @@ class Solution {
         int R = matrix.length;
         int C = matrix[0].length;
         int n = R*C;
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-
-        for(int i=0; i<R; i++) {
-            for(int j=0; j<C; j++) {
-                
-                pq.add(matrix[i][j]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>(n, new Comparator<Integer>() {
+            public int compare(Integer a, Integer b) {
+                return b - a;
             }
-        }
-
+        });
+        int num = n;
         int count = k;
-        int value = 0;
+        int row=0, col = 0;
         while(count > 0) {
-            // System.out.println(pq.peek());
-            value = pq.poll();
+            if(row < R && col < C) {
+                pq.add(matrix[row][col]);
+                col++;
+            }
+
+            if(col >= C) {
+                row++;
+                col = 0;
+            }
+            num--;
             count--;
         }
 
-        return value;
+        while(num > 0) {
+            if(row < R && col < C) {
+                if(matrix[row][col] < pq.peek()) {
+                    pq.poll();
+                    pq.add(matrix[row][col]);
+                }
+                col++;
+            }
+
+            if(col >= C) {
+                row++;
+                col = 0;
+            }
+            num--;
+        }
+
+        // while(!pq.isEmpty()) {
+        //     System.out.println(pq.poll());
+        // }
+
+        // for(int i=0; i<R; i++) {
+        //     for(int j=0; j<C; j++) {
+                
+        //         pq.add(matrix[i][j]);
+        //     }
+        // }
+
+        // int count = k;
+        // int value = 0;
+        // while(count > 0) {
+        //     // System.out.println(pq.peek());
+        //     value = pq.poll();
+        //     count--;
+        // }
+
+        return pq.poll();
     }
 }
