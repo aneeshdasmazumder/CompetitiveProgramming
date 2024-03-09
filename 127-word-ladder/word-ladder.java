ac299abce1df20db4a17ef1bf948266f02900249
Aneesh Das Mazumder <47806819+aneeshdasmazumder@users.noreply.github.com>
2024-03-09 15:53:33 +0530

    Time: 86 ms (64.60%) | Memory: 46 MB (73.61%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -4,30 +4,36 @@ class Solution {
             return 0;
        int cnt=0;
         Set<String> dict = new HashSet<>(wordList);
-        Queue<String> que = new LinkedList<>();
-
-        que.add(beginWord);
+        Queue<Pair> que = new LinkedList<>();
+        dict.remove(beginWord);
+        que.add(new Pair(beginWord, 1));
 
         while(!que.isEmpty()){
-            cnt++;
             int size = que.size();
-            for(int k=0; k<size; k++){
-                String word = que.poll();
-                if(word.equals(endWord))
-                    return cnt;
-                for(int i=0; i<word.length(); i++){
-                    for(char ch= 'a'; ch <= 'z'; ch++){
-                        char[] arr = word.toCharArray();
-                        arr[i] = ch;
-                        String midWord = new String(arr);
-                        if(dict.contains(midWord)){
-                            que.add(midWord);
-                            dict.remove(midWord);
-                        }
+            Pair pairedword = que.poll();
+            if(pairedword.word.equals(endWord))
+                return pairedword.step;
+            for(int i=0; i<pairedword.word.length(); i++){
+                for(char ch= 'a'; ch <= 'z'; ch++){
+                    char[] arr = pairedword.word.toCharArray();
+                    arr[i] = ch;
+                    String midWord = new String(arr);
+                    if(dict.contains(midWord)){
+                        que.add(new Pair(midWord, pairedword.step+1));
+                        dict.remove(midWord);
                     }
                 }
             }
         }
         return 0; 
+    }
+}
+
+class Pair {
+    String word;
+    int step;
+    Pair(String word, int step) {
+        this.word = word;
+        this.step = step;
     }
 }
