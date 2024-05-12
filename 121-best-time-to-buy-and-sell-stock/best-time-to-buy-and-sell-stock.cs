db294028d877d900cf0f9ef16e63042af8c630a9
Aneesh Das Mazumder <47806819+aneeshdasmazumder@users.noreply.github.com>
2024-05-12 15:34:32 +0530

    Time: 329 ms (91.16%) | Memory: 57.6 MB (78.59%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cs
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cs
@@ -1,25 +1,6 @@
 public class Solution {
     public int MaxProfit(int[] prices) {
         int maxProfit = 0;
-        /*
-        // Brute Force
-
-        for(int i=0; i<prices.Length-1; i++) {
-            int buy = prices[i];
-            int sell = prices[i+1];
-            for(int j=i+2; j<prices.Length; j++) {
-                if(prices[j] > sell) {
-                    sell = prices[j];
-                    Console.WriteLine(prices[j] + " " +sell);
-                }
-            }
-
-            if(sell > buy) {
-                int profit = sell - buy;
-                maxProfit = Math.Max(maxProfit, profit);
-            }
-        }
-        */
 
         // Optimized
         int buy = prices[0];
