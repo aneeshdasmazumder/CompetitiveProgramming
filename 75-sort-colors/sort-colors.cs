085182f6312510d21a0dad126c7a730850a8295f
Aneesh Das Mazumder <47806819+aneeshdasmazumder@users.noreply.github.com>
2024-05-15 23:33:39 +0530

    Time: 96 ms (66.78%) | Memory: 45.3 MB (70.43%) - LeetSync

--- 75-sort-colors/sort-colors.cs
+++ 75-sort-colors/sort-colors.cs
@@ -1,15 +1,17 @@
 public class Solution {
     public void SortColors(int[] nums) {
-        // Brute Force
-        int idx = 0;
-        for(int i=0; i<nums.Length; i++) {
-            for(int j=i+1; j<nums.Length; j++) {
-                if(nums[i] > nums[j]) {
+        int i=nums.Length - 1;
+        int c = 2;
+        while(i>=0 && c>=0) {
+            for(int j=i; j>=0; j--) {
+                if(nums[j]==c) {
                     int temp = nums[i];
                     nums[i] = nums[j];
                     nums[j] = temp;
+                    i--;
                 }
             }
+            c--;
         }
     }
 }
