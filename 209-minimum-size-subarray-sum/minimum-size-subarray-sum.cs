88f3769568f2aa03495283fa618633e1eb9f13c2
Aneesh Das Mazumder <47806819+aneeshdasmazumder@users.noreply.github.com>
2024-05-16 20:47:25 +0530

    Time: 158 ms (5.30%) | Memory: 51.4 MB (72.30%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cs
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cs
@@ -8,19 +8,17 @@ public class Solution {
         int minLen = Int32.MaxValue;
         while(right < nums.Length) {
             sum += nums[right];
-            if(sum >= target) {
-                minLen = Math.Min(minLen, (right - left) + 1);
-                // Console.WriteLine(right + " " +minLen + " " + sum);
-            }
+            // if(sum >= target) {
+            //     minLen = Math.Min(minLen, (right - left) + 1);
+            //     // Console.WriteLine(right + " " +minLen + " " + sum);
+            // }
                 
             
-            while(sum > target  && left < right) {
+            while(sum >= target) {
                 // Console.WriteLine(right + " " +sum);
+                minLen = Math.Min(minLen, (right - left) + 1);
                 sum -= nums[left];
                 left++;
-                if(sum >= target) {
-                    minLen = Math.Min(minLen, (right - left) + 1);
-                }
             }
 
             right++;
