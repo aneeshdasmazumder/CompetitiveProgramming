7e2fc2f9a59e100691cca33cad63b429538c0ffe
Aneesh Das Mazumder <47806819+aneeshdasmazumder@users.noreply.github.com>
2024-05-18 21:42:33 +0530

    Time: 98 ms (70.91%) | Memory: 46.3 MB (6.22%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public void Merge(int[] nums1, int m, int[] nums2, int n) {
+        if(n==0) return;
+        int nums1Len = nums1.Length-1;
+        int nums1Idx = nums1Len - n;
+        int nums2Idx = n-1;
+        Console.WriteLine(nums1Idx);
+        while(nums2Idx >= 0 && nums1Idx >= 0) {
+            // Console.WriteLine(nums1Idx + " " + nums2Idx);
+            if(nums1[nums1Idx] <= nums2[nums2Idx]) {
+                nums1[nums1Len] = nums2[nums2Idx];
+                // Console.WriteLine("nums1Len :" + nums1Len + " " + nums1[nums1Len]);
+                nums1Len--;
+                nums2Idx--;
+            } else {
+                nums1[nums1Len] = nums1[nums1Idx];
+                nums1[nums1Idx] = 0;
+                // Console.WriteLine("nums1LenElse :" + nums1Len + " " + nums1[nums1Len]);
+                nums1Idx--;
+                nums1Len--;
+            }
+        }
+
+        while(nums2Idx >= 0) {
+            nums1[nums1Len] = nums2[nums2Idx];
+            nums2Idx--;
+            nums1Len--;
+        }
+    }
+}
