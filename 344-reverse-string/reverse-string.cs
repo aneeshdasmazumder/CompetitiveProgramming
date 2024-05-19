f2a6cfae1428826b3f36b65e684670ff55ea798c
Aneesh Das Mazumder <47806819+aneeshdasmazumder@users.noreply.github.com>
2024-05-19 23:18:15 +0530

    Time: 229 ms (42.27%) | Memory: 129.4 MB (47.74%) - LeetSync

--- 344-reverse-string/reverse-string.cs
+++ 344-reverse-string/reverse-string.cs
@@ -1,29 +1,9 @@
 public class Solution {
     public void ReverseString(char[] s) {
-        int len = s.Length;
-
-        if(len%2==0)
-            evenArrayRev(s);
-        else oddArrayRev(s);
-    }
-
-    public void evenArrayRev(char[] s) {
         int left = 0;
         int right = s.Length - 1;
 
         while(left < right) {
-            char temp = s[left];
-            s[left] = s[right];
-            s[right] = temp;
-            left++; right--;
-        }
-    }
-
-    public void oddArrayRev(char[] s) {
-        int left = 0;
-        int right = s.Length - 1;
-        int mid = (right - left) / 2;
-        while(left<mid && right>mid) {
             char temp = s[left];
             s[left] = s[right];
             s[right] = temp;
