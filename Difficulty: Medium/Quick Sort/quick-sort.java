5d14a74f6c50cd8acf4c58e12db9ee45ae2195b1
Aneesh Das Mazumder <47806819+aneeshdasmazumder@users.noreply.github.com>
2026-05-07 22:09:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Quick Sort/quick-sort.java
+++ Difficulty: Medium/Quick Sort/quick-sort.java
@@ -3,26 +3,25 @@ class Solution {
         //System.out.println("low= "+low+" high= "+high);
         if(low < high) {
             int pi = partition(arr, low, high);
-            //System.out.println("pi = "+pi+" v= "+arr[pi]);
+
             quickSort(arr, low, pi-1);
             quickSort(arr, pi+1, high);
         }
     }
 
     private int partition(int[] arr, int low, int high) {
-        
-        int lb = low;
-        int rb = high;
-        
         int pivot = low;
-        while(lb <arr.length-1 && rb >= 0 && lb < rb) {
-            while(lb <arr.length-1 && arr[lb] <= arr[pivot]) lb++;
-            while(rb >= 0 && arr[rb] > arr[pivot]) rb--;
-            if(lb < rb) swap(arr, lb, rb);
+
+        while(low < arr.length && high >=0 && low < high) {
+            while(low < arr.length && arr[low] <= arr[pivot]) low++;
+            while(high >=0 && arr[high] > arr[pivot]) high--;
+
+            if(low < high) swap(arr, low, high);
         }
-        
-        swap(arr, pivot, rb);
-        return rb;
+
+        swap(arr, pivot, high);
+
+        return high;
     }
     
     private void swap(int[] arr, int low, int high) {
